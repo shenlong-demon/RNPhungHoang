bf6322dbfbb4851eb691445767afce85c81edbfa
VNDANLAP-0051\LONG <shenlongdemon@gmail.com>
2024-08-01 14:29:39 +0700

    calc percent for estimation

--- src/screens/portrait/main/pos/parts/OperationListItemView.tsx
+++ src/screens/portrait/main/pos/parts/OperationListItemView.tsx
@@ -1,9 +1,9 @@
-import {FC, memo, useEffect, useState} from 'react';
+import {FC, memo, useEffect, useMemo, useState} from 'react';
 import {Operation} from '@src/business';
 import {StyleSheet} from 'react-native';
 import View from '@core/components/viewbase/View';
 import Label from '@core/components/labelbase/Label';
-import {CONSTANTS, DateTimeUtils} from '@core/common';
+import {CONSTANTS, DateTimeUtils, Logger} from '@core/common';
 type Props = {
   operation: Operation;
   index: number;
@@ -18,19 +18,36 @@ export const OperationListItemView: FC<Props> = memo(
       }, 1000);
       return () => clearInterval(timer);
     }, []);
-    const donePercent: number = !operation.estimation ? 0 : (operation.estimation - operation.createdAt)
+    const donePercent = useMemo((): number => {
+      if (!operation.estimation) {
+        return 0;
+      }
+      const unit: number = 100 / (operation.estimation - operation.createdAt);
+      const percent : number =  ((time - operation.createdAt) * unit) / 100;
+      Logger.log(() => [
+        `OperationListItemView ${operation.name} unit ${unit}  done ${percent}`,
+      ]);
+      return percent;
+    }, [time]);
+
     return (
-      <View.Row
-        style={[
-          styles.container,
-          {
-            backgroundColor:
-              index % 2 === 0 ? 'rgba(234,252,234,0.37)' : 'white',
-          },
-        ]}
-        onPress={onPress}>
-        <Label.T text={operation.name || 'No name'} />
-      </View.Row>
+      <View.V>
+        <View.Row
+          style={[
+            styles.container,
+            {
+              backgroundColor:
+                index % 2 === 0 ? 'rgba(234,252,234,0.37)' : 'white',
+            },
+          ]}
+          onPress={onPress}>
+          <Label.T text={operation.name || 'No name'} />
+        </View.Row>
+        <View.V style={{height: 5, flexDirection: 'row'}}>
+          <View.V style={{flex: donePercent, backgroundColor: 'red'}} />
+          <View.V style={{flex: 1 - donePercent, backgroundColor: 'green'}} />
+        </View.V>
+      </View.V>
     );
   },
 );
