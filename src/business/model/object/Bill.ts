83e6923432243e94cafed0c1ef354d96094e15c7
VNDANLAP-0051\LONG <shenlongdemon@gmail.com>
2024-08-21 10:23:45 +0700

    display customer info in bill list item

--- src/business/model/object/Bill.ts
+++ src/business/model/object/Bill.ts
@@ -1,11 +1,13 @@
 import { BaseModel } from '@src/business/model/object/base_model';
 import { BillIssue } from '@src/business/model/object/BillIssue';
 import { Order } from '@src/business/model/object/Order';
+import { Customer } from '@src/business';
 
 export type Bill = BaseModel & {
   name?: string;
   total: number;
   receiptedAt: number;
+  customer?: Customer | null;
   discount: number;
   orders: Order[];
   issues: BillIssue[];
--- src/screens/portrait/main/bill/parts/BillListItemView.tsx
+++ src/screens/portrait/main/bill/parts/BillListItemView.tsx
@@ -13,34 +13,58 @@ type Props = {
 export const BillListItemView: FC<Props> = memo(
   ({ item, index, onPress }: Props) => {
     return (
-      <View.Row
+      <View.V
         onPress={onPress}
-        style={[
-          styles.container,
-          {
-            backgroundColor:
-              index % 2 === 0 ? 'rgba(234,252,234,0.37)' : 'white',
-          },
-        ]}>
+        style={{
+          flexDirection: 'row',
+          backgroundColor: index % 2 === 0 ? 'rgba(234,252,234,0.37)' : 'white',
+          borderColor: 'green',
+          borderBottomWidth: 1,
+        }}>
         <Label.T style={styles.billNo} text={`${item.id}`} />
-        <Label.T style={styles.name} text={item.name || 'No name'} />
-        <Label.T
-          style={styles.date}
-          text={DateTimeUtils.formatDateTimeString(item.receiptedAt)}
-        />
-        <Label.Money style={styles.price} value={item.total} />
-      </View.Row>
+        <View.V style={[styles.container]}>
+          <View.Row style={{}}>
+            <Label.T style={styles.name} text={item.name || 'No name'} />
+            <Label.T
+              style={styles.date}
+              text={DateTimeUtils.formatDateTimeString(item.receiptedAt)}
+            />
+            <Label.Money style={styles.price} value={item.total} />
+          </View.Row>
+          {!!item.customer ? (
+            <View.V style={styles.customerContainer}>
+              <Label.T
+                style={styles.customerName}
+                text={`${item.customer.name} - ${item.customer.phone}`}
+              />
+            </View.V>
+          ) : null}
+        </View.V>
+      </View.V>
     );
   },
 );
 const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1 },
   billNo: {
-    flex: 2,
-    marginRight: 5,
+    width: 70,
+    fontWeight: 'bold',
   },
   name: {
     flex: 4,
+    fontWeight: 'bold',
+  },
+  customerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+  },
+  customerName: {
+    flex: 1,
+    fontWeight: 'bold',
+    color: 'green',
+    textAlign: 'right',
   },
   date: {
     flex: 4,
@@ -48,5 +72,6 @@ const styles = StyleSheet.create({
   price: {
     flex: 4,
     textAlign: 'right',
+    fontWeight: 'bold',
   },
 });
